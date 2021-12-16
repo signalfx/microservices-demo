79348f3264e4e8784873c76924a97ea2983ec573
Jason Plumb <jplumb@splunk.com>
2021-12-15 16:00:12 -0800

    abort after timeout

--- src/adservice/src/main/java/hipstershop/copyright/StockPhotos.java
+++ src/adservice/src/main/java/hipstershop/copyright/StockPhotos.java
@@ -1,6 +1,9 @@
 package hipstershop.copyright;
 
+import hipstershop.AdService;
 import hipstershop.Demo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -8,13 +11,20 @@ import java.util.List;
 // A massive repository of very many photos.
 public class StockPhotos {
 
+    private final static long MAX_TIME_MS = 6500;
     private final static List<CopyrightPhoto> photos = createDatabase();
+    private static final Logger logger = LogManager.getLogger(AdService.class);
 
     boolean isCopyright(Demo.Ad ad){
         boolean result = true;
+        long start = System.currentTimeMillis();
         for (CopyrightPhoto photo : photos) {
             if(photo.matchesAd(ad)){
                 result = false;
+            }
+            if(System.currentTimeMillis() - start > MAX_TIME_MS){
+                logger.warn("Copyright check exceeded max threshold. Aborting.");
+                break;
             }
         }
         return result;
