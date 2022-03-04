81cad031ae3bcd93770368c85c3215199aeed642
sonu0610 <sonu.jain@crestsysdata.com>
2022-03-04 13:23:38 +0530

    changed endpoint names.

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -138,9 +138,9 @@ func main() {
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	r.HandleFunc("/robots.txt", func(w http.ResponseWriter, _ *http.Request) { fmt.Fprint(w, "User-agent: *\nDisallow: /") })
 	r.HandleFunc("/_healthz", func(w http.ResponseWriter, _ *http.Request) { fmt.Fprint(w, "ok") })
-	r.HandleFunc("/generatePayment", svc.generatePaymentHandler).Methods(http.MethodPost)
-	r.HandleFunc("/generateSalesTax", svc.generateSalesTaxHandler).Methods(http.MethodGet)
-	r.HandleFunc("/generateCartEmpty", svc.generateCartEmptyHandler).Methods(http.MethodGet)
+	r.HandleFunc("/payment/send", svc.generatePaymentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/cart/calculateTax", svc.generateSalesTaxHandler).Methods(http.MethodGet)
+	r.HandleFunc("/cart/clear", svc.generateCartEmptyHandler).Methods(http.MethodGet)
 
 	var handler http.Handler = r
 	handler = &logHandler{log: log, next: handler} // add logging
